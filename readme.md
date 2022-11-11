61ea11d76096e696099eb9249ddc627b33336159
usmc1371nerd <james.p.dumas@gmail.com>
2022-11-11 16:38:13 -0600

    Init

---
+++ readme.md
@@ -0,0 +1,26 @@
+This is a web application as a way to document shooting notes. I would like to eventually build this out to be a app that can be put on a phone. This is why I chose python for its ability to be able to handle a lot of computing data. The coding language is easy to learn as well and I can easily find help in the future.
+
+The frameworks and languages used for this site is:
+
+Python (used to extend websites templates to other places and where you see {%%} if and else statments as well)
+HTML (used to code base.html)
+CSS (addtional styling other than bootstrap set templates)
+Bootstrap (a template)
+JavaScript (used to call a delete function from the database)
+SQL (database)
+JSON (calling a delete functions and moving data for sign-up and logins)
+
+The something new I learned:
+
+Adding a sign-up page we did learn a log-in but a sign-up we did not.
+
+JINJA2 (Templating language this is also my something new learned)
+
+For now I new I had to build a smaller app with some short of templating ability as first so I could manage this project on my own. I didn't make any wire frames but instead wrote and drew small little ideas to help drive my organization on my ideas. But with the timeline I knew in order to push a MVP to at least get the idea started. I would have to keep it simple so just a login and signup to control the info of each user and note taking for now will do. So to solve all these problems bootstrap, and jinja seemed like a great fit. I would then do any additional styling using CSS. 
+
+Login for bottega:
+Email: 123@123
+Password:Bottega12 
+
+
+
---
+++ website/views.py
@@ -0,0 +1,71 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
+
+
+views = Blueprint('views', __name__)
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 8:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id= current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template("notes.html", user=current_user)
+# using JSON to do a request
+@views.route('/delete-note', methods=[ 'POST' ])
+def delete_note():
+   note = json.loads(request.data)
+   noteId = note['noteId']
+   note = Note.query.get(noteId)  
+   if note:
+    if note.user_id == current_user.id:
+        db.session.delete(note)
+        db.session.commit()
+    
+    return jsonify({})
+
+
+# bzo = Blueprint('/bzo', __name__)
+
+# @views.route ('/bzo', methods=['GET', 'POST'])   
+# @login_required
+# def nothome():
+#     return render_template("bzo.html", user=current_user)
+
+# #     if request.method == 'POST':
+# #         bzonote = request.form.get('bzonote')
+
+# #         if len(bzonote) < 8:
+# #             flash('Note is too short!', category='error')
+# #         else:
+# #             new_note = Bzonote(data=bzonote, user_id= current_user.id)
+# #             db.session.add(new_note)
+# #             db.session.commit()
+# #             flash('Note added!', category='success')
+
+    
+#     return render_template("bzo.html", user=current_user)
+
+# #     @views.route('/delete-notebzo', methods=[ 'POST' ])
+# #     def delete_note():
+# #         bzonote = json.loads(request.data)
+# #         bzonoteId = bzonote['noteId']
+# #         bzonote = Bzonote.query.get(noteId)  
+# #         if bzonote:
+# #             if bzonote.user_id == current_user.id:
+# #                 db.session.delete(note)
+# #                 db.session.commit()
+            
+# #             return jsonify({})
+
